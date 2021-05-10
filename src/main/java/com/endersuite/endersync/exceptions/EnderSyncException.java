8a6955df93c903e04b49f626bfc7b5eb5716cdbd
maximilianheidenreich <github@maximilian-heidenreich.de>
2021-05-10 22:50:02 +0200

    Adds custom exceptions

---
+++ src/main/java/com/endersuite/endersync/exceptions/EnderSyncException.java
@@ -0,0 +1,9 @@
+package com.endersuite.endersync.exceptions;
+
+/**
+ * The parent Exception class all exceptions thrown inside endersync need to inherit from!
+ *
+ * @author Maximilian Vincent Heidenreich
+ * @since 10.05.21
+ */
+public class EnderSyncException extends Exception {}
